a270284efb5074bac6f57e74eea4e4ac7b3f26a2
alexshakouri <acshakouri@ucdavis.edu>
2020-07-18 16:27:26 -0700

    fix header guards and extra library calls

--- initialization.h
+++ initialization.h
@@ -12,4 +12,4 @@ xmlNodePtr xml_setup(std::string html_buffer);
 int curl_write(char* data, size_t size, size_t nmemb, std::string *write_data);
 CURLcode curl_initialization(CURL* curl_connection, const char *URL_name, std::string *html_buffer);
 
-#endif INITIALIZATION_H
+#endif //INITIALIZATION_H
